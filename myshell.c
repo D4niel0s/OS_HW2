036421b6b7ecac93a9a0d9f4afe40c6dbc7f44c9
D4niel0s <das.volkov@gmail.com>
2024-07-03 18:54:04 +0300

    Added checks to syscalls

--- myshell.c
+++ myshell.c
@@ -33,7 +33,7 @@ int finalize(void){
 // RETURNS - 1 if should continue, 0 otherwise
 int process_arglist(int count, char** arglist){
     
-    int i, pid, status, retVal;
+    int i, pid, retVal;
 
 
     if(strcmp(arglist[count-1], "&") == 0){
@@ -53,37 +53,36 @@ int process_arglist(int count, char** arglist){
     
     /*Regular command*/
     pid = fork();
-    switch(pid){
-        case 0: /*Child*/
-            signal(SIGINT, SIG_DFL);
+    if(pid < 0){
+        perror("Error: forking failed\n");
+        return 0;
 
-            retVal = execvp(arglist[0], arglist);
-            if(retVal == -1){
-                perror("Error: couldn't execute command.\n");
-                exit(1);
+    }else if(pid == 0){ /*Child*/
+        signal(SIGINT, SIG_DFL);
+
+        retVal = execvp(arglist[0], arglist);
+        if(retVal == -1){
+            perror("Error: couldn't execute command.\n");
+            exit(1);
+        }
+
+    }else{ /*Parent*/
+        if(waitpid(pid, NULL, 0) == -1){
+            switch(errno){
+                case ECHILD:
+                case EINTR:
+                    break;
+                default:
+                    return 0;
             }
-            break;
-        case -1: /*Error*/
-            return 0;
-        default: /*Parent*/
-            if(waitpid(pid, &status, 0) == -1){
-                switch(errno){
-                    case ECHILD:
-                    case EINTR:
-                        break;
-                    default:
-                        return 0;
-                }
-            }
-            break;
+        }
     }
-
     return 1;
 }
 
 
 int runAppendComm(int appInd, char **args){
-    int fd, pid, retVal;
+    int fd, pid, retVal,dupRet, clsRet;
 
     args[appInd] = NULL;
 
@@ -96,16 +95,24 @@ int runAppendComm(int appInd, char **args){
     }else if(pid == 0){ /*Child*/
         signal(SIGINT, SIG_DFL);
         fd = open(args[appInd+1], O_WRONLY | O_APPEND | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
-
         if(fd<0){
             perror("Error: could not open file\n");
             exit(1);
         }
 
-        dup2(fd, STDOUT_FILENO);
-        close(fd);
-        retVal = execvp(args[0],args);
+        dupRet = dup2(fd, STDOUT_FILENO);
+        if(dupRet < 0){
+            perror("Error: could not redirect file descriptor\n");
+            exit(1);
+        }
 
+        clsRet = close(fd);
+        if(clsRet < 0){
+            perror("Error: failed closing file\n");
+            exit(1);
+        }
+
+        retVal = execvp(args[0],args);
         if(retVal < 0){
             perror("Error: could not execute command\n");
             exit(1);
@@ -119,7 +126,7 @@ int runAppendComm(int appInd, char **args){
 }
 
 int runRedirectComm(int revInd, char **args){
-    int fd, pid, retVal;
+    int fd, pid, retVal,dupRet, clsRet;
 
     args[revInd] = NULL;
 
@@ -132,16 +139,24 @@ int runRedirectComm(int revInd, char **args){
     }else if(pid == 0){ /*Child*/
         signal(SIGINT, SIG_DFL);
         fd = open(args[revInd+1], O_RDONLY);
-
         if(fd<0){
             perror("Error: could not open file\n");
             exit(1);
         }
 
-        dup2(fd, STDIN_FILENO);
-        close(fd);
-        retVal = execvp(args[0],args);
+        dupRet = dup2(fd, STDIN_FILENO);
+        if(dupRet < 0){
+            perror("Error: could not redirect file descriptor\n");
+            exit(1);
+        }
 
+        clsRet = close(fd);
+        if(clsRet < 0){
+            perror("Error: failed closing file\n");
+            exit(1);
+        }
+
+        retVal = execvp(args[0],args);
         if(retVal < 0){
             perror("Error: could not execute command\n");
             exit(1);
@@ -155,7 +170,7 @@ int runRedirectComm(int revInd, char **args){
 }
 
 int runPipeComm(int pipeInd, char**args){
-    int pid1,pid2, retVal;
+    int pid1,pid2, retVal,dupRet, clsRet;
     int pfds[2];
 
     args[pipeInd] = NULL;
@@ -165,17 +180,31 @@ int runPipeComm(int pipeInd, char**args){
         return 0;
     }
 
-
-    if((pid1 = fork()) < 0){
+    pid1 = fork();
+    if(pid1 < 0){
         perror("Error: forking failed.\n");
         return 0;
 
     }else if(pid1 == 0){ /*Left side command*/
         signal(SIGINT, SIG_DFL);
 
-        close(pfds[0]);
-        dup2(pfds[1], STDOUT_FILENO);
-        close(pfds[1]);
+        clsRet = close(pfds[0]);
+        if(clsRet < 0){
+            perror("Error: failed closing file\n");
+            exit(1);
+        }
+
+        dupRet = dup2(pfds[1], STDOUT_FILENO);
+        if(dupRet < 0){
+            perror("Error: could not redirect file descriptor\n");
+            exit(1);
+        }
+
+        clsRet = close(pfds[1]);
+        if(clsRet < 0){
+            perror("Error: failed closing file\n");
+            exit(1);
+        }
 
         retVal = execvp(args[0], args);
         if(retVal<0){
@@ -184,16 +213,31 @@ int runPipeComm(int pipeInd, char**args){
         }
     
     }else{ /*Parent*/
-        if((pid2 = fork()) < 0){
+        pid2 = fork();
+        if(pid2 < 0){
             perror("Error: forking failed.\n");
             return 0;
 
         }else if(pid2 == 0){ /*Right side command*/
             signal(SIGINT, SIG_DFL);
 
-            close(pfds[1]);
-            dup2(pfds[0], STDIN_FILENO);
-            close(pfds[0]);
+            clsRet = close(pfds[1]);
+            if(clsRet < 0){
+                perror("Error: failed closing file\n");
+                exit(1);
+            }
+
+            dupRet = dup2(pfds[0], STDIN_FILENO);
+            if(dupRet < 0){
+                perror("Error: could not redirect file descriptor\n");
+                exit(1);
+            }
+
+            clsRet = close(pfds[0]);
+            if(clsRet < 0){
+                perror("Error: failed closing file\n");
+                exit(1);
+            }
 
             retVal = execvp(args[pipeInd+1], args+pipeInd+1);
             if(retVal<0){
@@ -201,8 +245,17 @@ int runPipeComm(int pipeInd, char**args){
                 exit(1);
             }
         }else{ /*Parent*/
-            close(pfds[0]);
-            close(pfds[1]);
+            clsRet = close(pfds[0]);
+            if(clsRet < 0){
+                perror("Error: failed closing file\n");
+                exit(1);
+            }
+
+            clsRet = close(pfds[1]);
+            if(clsRet < 0){
+                perror("Error: failed closing file\n");
+                exit(1);
+            }
             while(wait(NULL)>0);
         }
     }
@@ -232,7 +285,3 @@ int runCommInBG(int count, char**args){
     /*Parent does nothing here*/
     return 1;
 }
-
-
-
-
